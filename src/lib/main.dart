7bc04e4c62afde814f3dde4ce8977e9eebcc0ae9
André Luiz <an3dree@gmail.com>
2022-06-13 17:37:57 -0300

    initialize firebase and named routes

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:assistant_learn_up/pages/assistant_quiz_page.dart';
+import 'package:assistant_learn_up/pages/learn_page.dart';
+import 'package:assistant_learn_up/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+//import 'firebase_options';
+import 'pages/register_page.dart';
 import 'pages/sound_quiz_page.dart';
 import 'pages/home.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -14,7 +22,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Assistant Learn\'Up',
       theme: ThemeData.dark().copyWith(primaryColor: Colors.teal),
-      home: const HomePage(),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/registration': (context) => RegistrationPage(),
+        '/home': (context) => HomePage(),
+        '/sound_quiz': (context) => SoundQuestionPage(),
+        '/assistant_quiz': (context) => AssistantQuizPage(),
+        '/learn': (context) => LearnAssistantPage()
+      },
     );
   }
 }
