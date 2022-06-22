0607dbef661df3cd34312183a8e8cd2090fadf57
André Luiz <an3dree@gmail.com>
2022-06-21 22:20:20 -0300

    add spiner

--- src/lib/pages/login_page.dart
+++ src/lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../utils/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -13,97 +14,115 @@ class _LoginPageState extends State<LoginPage> {
   late String email;
   late String password;
   final auth = FirebaseAuth.instance;
+  bool _loading = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          SizedBox(
-            height: 100,
-          ),
-          Container(
-            height: 130,
-            child: Image.asset('assets/images/playstore.png'),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          TextField(
-            keyboardType: TextInputType.emailAddress,
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.black),
-            onChanged: (value) {
-              email = value;
-            },
-            decoration: kInputDecoration.copyWith(hintText: 'Digite seu Email'),
-          ),
-          SizedBox(
-            height: 8.0,
-          ),
-          TextField(
-            obscureText: true,
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.black),
-            onChanged: (value) {
-              password = value;
-            },
-            decoration: kInputDecoration.copyWith(hintText: 'Digite sua Senha'),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: TextButton(
-              style: TextButton.styleFrom(backgroundColor: Colors.lightBlue),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Entrar',
-                  style: TextStyle(color: Colors.black87, fontSize: 18),
+      body: ModalProgressHUD(
+        inAsyncCall: _loading,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+            Container(
+              height: 130,
+              child: Image.asset('assets/images/playstore.png'),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.black),
+              onChanged: (value) {
+                email = value;
+              },
+              decoration:
+                  kInputDecoration.copyWith(hintText: 'Digite seu Email'),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              obscureText: true,
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.black),
+              onChanged: (value) {
+                password = value;
+              },
+              decoration:
+                  kInputDecoration.copyWith(hintText: 'Digite sua Senha'),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.lightBlue),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Entrar',
+                    style: TextStyle(color: Colors.black87, fontSize: 18),
+                  ),
                 ),
-              ),
-              onPressed: () async {
-                try {
-                  final user = await auth.signInWithEmailAndPassword(
-                      email: email, password: password);
-                  if (user != null) {
-                    Navigator.pushNamed(context, '/home');
+                onPressed: () async {
+                  setState(() {
+                    _loading = true;
+                  });
+                  try {
+                    final user = await auth.signInWithEmailAndPassword(
+                        email: email, password: password);
+                    if (user != null) {
+                      Navigator.pushNamed(context, '/home');
+                      setState(() {
+                        _loading = false;
+                      });
+                    }
+                  } catch (e) {
+                    print(e);
                   }
-                } catch (e) {
-                  print(e);
-                }
-              },
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            'Ainda não possui cadstro?',
-            textAlign: TextAlign.center,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: TextButton(
-              style: TextButton.styleFrom(backgroundColor: Colors.lightGreen),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Registrar',
-                  style: TextStyle(color: Colors.black87, fontSize: 18),
-                ),
+                },
               ),
-              onPressed: () {
-                Navigator.pushNamed(context, '/registration');
-              },
             ),
-          ),
-        ],
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Ainda não possui cadstro?',
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.lightGreen),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Registrar',
+                    style: TextStyle(color: Colors.black87, fontSize: 18),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/registration');
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- src/lib/pages/register_page.dart
+++ src/lib/pages/register_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import '../utils/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 class RegistrationPage extends StatefulWidget {
   const RegistrationPage({Key? key}) : super(key: key);
@@ -14,85 +16,118 @@ class _RegistrationPageState extends State<RegistrationPage> {
   late String email;
   late String password;
   final auth = FirebaseAuth.instance;
+  bool _saving = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          SizedBox(
-            height: 100,
-          ),
-          Container(
-            height: 130,
-            child: Image.asset('assets/images/playstore.png'),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          TextField(
-            keyboardType: TextInputType.emailAddress,
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.black),
-            onChanged: (value) {
-              displayName = value;
-            },
-            decoration: kInputDecoration.copyWith(hintText: 'Digite seu Nome'),
-          ),
-          SizedBox(
-            height: 8.0,
-          ),
-          TextField(
-            keyboardType: TextInputType.emailAddress,
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.black),
-            onChanged: (value) {
-              email = value;
-            },
-            decoration: kInputDecoration.copyWith(hintText: 'Digite seu Email'),
-          ),
-          SizedBox(
-            height: 8.0,
-          ),
-          TextField(
-            obscureText: true,
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.black),
-            onChanged: (value) {
-              password = value;
-            },
-            decoration: kInputDecoration.copyWith(hintText: 'Digite sua Senha'),
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: TextButton(
-              style: TextButton.styleFrom(backgroundColor: Colors.lightBlue),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Cadastrar',
-                  style: TextStyle(color: Colors.black87, fontSize: 18),
-                ),
-              ),
-              onPressed: () async {
-                try {
-                  var user;
-                  await auth
-                      .createUserWithEmailAndPassword(
-                          email: email, password: password)
-                      .then((user) =>
-                          auth.currentUser?.updateDisplayName(displayName));
-                } catch (e) {
-                  print(e);
-                }
-              },
+      body: ModalProgressHUD(
+        inAsyncCall: _saving,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(
+              height: 100,
             ),
-          ),
-        ],
+            Container(
+              height: 130,
+              child: Image.asset('assets/images/playstore.png'),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.black),
+              onChanged: (value) {
+                displayName = value;
+              },
+              decoration:
+                  kInputDecoration.copyWith(hintText: 'Digite seu Nome'),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.black),
+              onChanged: (value) {
+                email = value;
+              },
+              decoration:
+                  kInputDecoration.copyWith(hintText: 'Digite seu Email'),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              obscureText: true,
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.black),
+              onChanged: (value) {
+                password = value;
+              },
+              decoration:
+                  kInputDecoration.copyWith(hintText: 'Digite sua Senha'),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.lightBlue),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Cadastrar',
+                    style: TextStyle(color: Colors.black87, fontSize: 18),
+                  ),
+                ),
+                onPressed: () async {
+                  setState(() {
+                    _saving = true;
+                  });
+                  try {
+                    var user;
+                    await auth
+                        .createUserWithEmailAndPassword(
+                            email: email, password: password)
+                        .then((user) =>
+                            auth.currentUser?.updateDisplayName(displayName));
+                    Navigator.popAndPushNamed(context, '/login');
+                    setState(() {
+                      _saving = false;
+                    });
+                  } catch (e) {
+                    print(e);
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  _onAlertButtonPressed(context) {
+    Alert(
+      context: context,
+      type: AlertType.error,
+      title: "RFLUTTER ALERT",
+      desc: "Flutter is more awesome with RFlutter Alert.",
+      buttons: [
+        DialogButton(
+          child: Text(
+            "COOL",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () => Navigator.popAndPushNamed(context, '/login'),
+          width: 120,
+        )
+      ],
+    ).show();
   }
 }
